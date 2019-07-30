3671937befdaa3d1f3fd40827878a634ce8b56fb
falken141 <49424108+falken141@users.noreply.github.com>
2019-07-30 16:20:45 +0900

    Add files via upload

---
+++ acb.js
@@ -0,0 +1,20 @@
+﻿function openWin1() {
+    myWindow = window.open("", "myWindow", "width=300,height=200");
+    myWindow.document.write("<dl><dt>萬子<dt/><dd>読み方　マンズ</dd><dd>ローカル役ではこれでしか組めないものもある</dd></dl>");
+}
+
+function openWin2() {
+    myWindow = window.open("", "myWindow", "width=300,height=200");
+    myWindow.document.write("<dl><dt>筒子<dt/><dd>読み方　ピンズ</dd><dd>ローカル役ではこれでしか組めないものもある</dd></dl>");
+}
+
+function openWin3() {
+    myWindow = window.open("", "myWindow", "width=300,height=200");
+    myWindow.document.write("<dl><dt>索子<dt/><dd>読み方　ソウズ</dd><dd>ローカル役ではこれでしか組めないものもある</dd></dl>");
+}
+
+function openWin7() {
+    myWindow = window.open("", "myWindow", "width=300,height=200");
+    myWindow.document.write("<dl><dt>三元牌<dt/><dd>読み方　サンゲンパイ</dd><dd>白（ハク）發（ハツ）中（チュン）がある</dd></dl>");
+}
+
---
+++ dfe.js
@@ -0,0 +1,9 @@
+﻿function openWin8() {
+    myWindow = window.open("", "myWindow", "width=300,height=200");
+    myWindow.document.write("<dl><dt>四風牌<dt/><dd>読み方　スーフォンパイ</dd><dd>東（トン）南（ナン）西（シャー）北（ペー）がある</dd></dl>");
+}
+
+function openWin9() {
+    myWindow = window.open("", "myWindow", "width=300,height=200");
+    myWindow.document.write("<dl><dt>赤牌<dt/><dd>読み方　アカハイ（ドラハイ）</dd><dd>使うとドラがのる</dd></dl>");
+}
